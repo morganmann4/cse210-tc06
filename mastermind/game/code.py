1d022c676f67733d84c86f8fd76b8f385eb2efff
Morgan Mann <momomann4@gmail.com>
2021-05-29 21:27:10 -0600

    pytest changes

--- mastermind/game/code.py
+++ mastermind/game/code.py
@@ -4,7 +4,7 @@ class Code:
     """collects random numbers from 1000 to 9999"""
 
     def __init__(self):
-     """Gets numerical input from the user through the screen.
+        """Gets numerical input from the user through the screen.
 
         Args: 
             self (Screen): An instance of Screen.
@@ -12,15 +12,15 @@ class Code:
         Returns:
             integer: The user's input as an integer.
         """
-     self.random_num = random.randint(1000, 9999)
+        self.random_num = random.randint(1000, 9999)
 
     def get_random_num(self):
-     """Gets numerical input from the user through the screen.
+        """Gets numerical input from the user through the screen.
           
         Args: 
             self (Screen): An instance of Screen.
        Returns:
             integer: The user's input as an random integer.
         """
-    #print(self.random_num)
-    return str(self.random_num)
+        #print(self.random_num)
+        return str(self.random_num)
--- mastermind/test_pyest.py
+++ mastermind/test_pyest.py
@@ -4,6 +4,7 @@ from game.player import Player
 from game.director import Director
 from game.code import Code
 from game.guess import Guess
+from game.check import Check
 
 
 
@@ -21,9 +22,12 @@ def test_guess_lists():
         assert guesses.player_guess == ['1', '1', '2', '3']
         assert guesses.answer == ['1', '2', '3', '4']
 
-
-
-
+def test_check():
+    check = Check()
+    guess = ["2", "4", "6", "8"]
+    answer = ["2", "3", "8", "9"]
+    check.check(guess, answer)
+    assert output == ["x", "*", "o", "*"]
 
 
 py.main(["-v", "--tb=no", "test_pytest.py"]) 
